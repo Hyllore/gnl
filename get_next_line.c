2f388f982c8dc8b2f9ddf6b231c468c16ae5a85e
Dorian ROLY <droly@e1r7p13.42.fr>
2016-01-07 17:58:07 +0100

    envoi renouveau

--- get_next_line.c
+++ get_next_line.c
@@ -6,91 +6,71 @@
 /*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 11:55:39 by droly             #+#    #+#             */
-/*   Updated: 2016/01/03 16:21:37 by droly            ###   ########.fr       */
+/*   Updated: 2016/01/07 17:57:34 by droly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	length_file(int fd)
-{
-	int		size;
-	int		ret;
-	char	buf[BUFF_SIZE + 1];
-
-	size = 0;
-	ret = 1;
-	while (ret > 0)
-	{
-		ft_bzero(buf, BUFF_SIZE);
-		ret = read(fd, buf, BUFF_SIZE);
-		size += ret;
-	}
-	return (ret == -1 ? 0 : size);
-}
-
-static char	**get_file(int fd, int size)
-{
-	int		i;
-	int		j;
-	int		ret;
-	char	buf[BUFF_SIZE + 1];
-	char	**str;
-
-	i = 0;
-	ret = 1;
-	str[1] = NULL;
-//	ft_putnbr(size);
-	str[0] = malloc(sizeof(char*) * (size));
-	while (ret > 0)
-	{
-		ft_bzero(buf, BUFF_SIZE);
-		ret = read(fd, buf, BUFF_SIZE);
-		j = 0;
-//		ft_putchar('8');
-		while (buf[j] != '\0')
-		{
-			str[0][i] = buf[j];
-			i++;
-			j++;
-		}
-	}
-	ft_putnbr(i);
-	str[0][i] = '\0';
-//	ft_putstr(str[0]);
-	return (ret == -1 ? NULL : str);
-}
-
 int				get_next_line(int const fd, char **line)
 {
-	static int	i;
-	int			ret;
-//	ft_putnbr(i);
-	while (line[0][i] != '\n' || line[0][i] != '\0')
+	static char *str;
+	int ret;
+	char *tmp;
+	int i;
+
+	i = 1;
+	tmp = NULL;
+	ret = 0;
+	if (str == NULL)
 	{
-		ft_putchar(line[0][i]);
-		i++;
+		ft_putchar('1');
+		*line = ft_strnew(BUFF_SIZE);
+		ft_putchar('2');
+		ft_putchar('3');
+		ret = read(fd, *line, BUFF_SIZE);
+		ft_putchar('4');
+		(*line)[BUFF_SIZE + 1] = '\0';
+		ft_putchar('5');
+		ft_putnbr(ft_strlen(*line));
+		while (ft_strchr(*line, 10) == NULL && BUFF_SIZE * i == ft_strlen(*line))
+		{
+			ft_putchar('6');
+			tmp = ft_strnew(BUFF_SIZE);
+			ret = read(fd, tmp, BUFF_SIZE);
+			ft_strjoin(*line, tmp);
+			i++;
+		}
 	}
-	i++;
-	ft_putchar('\n');
-	return (i);
+	if (str != NULL)
+	{
+		*line = ft_strnew(BUFF_SIZE + ft_strlen(str) - 1);
+		ret = read(fd, *line, BUFF_SIZE);
+		*line = ft_strjoin(str, *line);
+		(*line)[BUFF_SIZE + ft_strlen(str)] = '\0';
+		if (ft_strchr(*line, 10) == NULL && BUFF_SIZE + ft_strlen(str) ==
+				ft_strlen(*line))
+		{
+			str = ft_strnew(ft_strlen(*line) - 1);
+			str = ft_strcpy(str, *line);
+			return (1);
+		}
+	}
+	return (1);
 }
 
 int main(int argc, char **argv)
 {
 	int fd;
-	char *file;
-	int fd2;
-	char **str;
-	int size;
+	char *line;
 
-	file = argv[1];
+	line = NULL;
 	if (argc != 2)
 		return(0);
-	fd = open(file , O_RDONLY);
-	fd2 = open(file, O_RDONLY);
-	size = length_file(fd);
-	str = get_file(fd2, size);
-	while (fd)
-		get_next_line(fd, str);
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+	{
+		sleep(1);
+		ft_putstr(line);
+	}
 }
